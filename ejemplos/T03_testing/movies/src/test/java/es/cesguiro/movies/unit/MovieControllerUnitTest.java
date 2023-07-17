366a52a2ea69049d480558ffc56e6955ca702fa3
César Guijarro <cesar.rosaleny@gmail.com>
2023-07-17 13:56:31 +0200

    Añadido T02 - Testing

--- ejemplos/T03_testing/movies/src/test/java/es/cesguiro/movies/unit/MovieControllerUnitTest.java
+++ ejemplos/T03_testing/movies/src/test/java/es/cesguiro/movies/unit/MovieControllerUnitTest.java
@@ -6,6 +6,8 @@ import es.cesguiro.movies.domain.service.MovieService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -31,8 +34,22 @@ public class MovieControllerUnitTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /*@Autowired
+    private Mock mock;*/
+
+    //@Mock
     @MockBean
     private MovieService movieService;
+
+    private List<Movie> movies = List.of(
+            new Movie(1, "Cadena perpetua", 1994, 142),
+            new Movie(2, "El padrino", 1972, 175)
+            );
+    /*
+    * ¿Sustituye el Bean MovieService por éste?
+    */
+    /*@MockBean
+    private MovieService movieService;*/
 
     private Movie movie;
 
@@ -41,16 +58,18 @@ public class MovieControllerUnitTest {
 
     @DisplayName("Test getAll()")
     @Test
-    public void testGetMoviesList(){
-        when(movieService.getAll()).thenReturn(Collections.singletonList(movie));
+    public void testGetMoviesList() throws Exception {
+        //when(movieService.getAll()).thenReturn(Collections.singletonList(movie));
+        when(movieService.getAll()).thenReturn(this.movies);
+        //mock.
         mockMvc.perform(get("/movies"))
                         .andDo(print())
                         .andExpect(status().isOk())
-                        .andExpect(content().contentType(List<Movie>).class)
-        assertEquals(movies, this.movieService.getAll());
+                        .andExpect(content().contentType("application/json"));
+        //assertEquals(movies, this.movieService.getAll());
     }
 
-    @Test
+    /*@Test
     public void testGetOrdersList() throws Exception {
         when(orderService.getOrders()).thenReturn(Collections.singletonList(order));
         mockMvc.perform(get("/api/orders"))
@@ -59,6 +78,6 @@ public class MovieControllerUnitTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$").isArray());
-    }
+    }*/
 
 }
