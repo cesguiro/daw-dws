1403f3394be8806a6dd77e829253652ae8ea29d2
cguijarro <cesguiro@gmail.com>
2023-12-05 08:09:04 +0100

    Cambiado proyecto final

--- ejemplos/proyecto_final/movies/src/main/java/es/cesguiro/movies/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package es.cesguiro.movies.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    private static final String DESCRIPTION = "Resource not found";
-
-    public ResourceNotFoundException(String message) {
-        super(DESCRIPTION + ". " + message);
-    }
-}
